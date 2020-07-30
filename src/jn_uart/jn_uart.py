194a8a348f87c434a46215eb1a41b9ebca7e7865
jin <jinparksj@gmail.com>
2020-07-30 13:18:13 -0700

    merged

--- src/jn_uart/jn_uart.py
+++ src/jn_uart/jn_uart.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-sp = serial.Serial(port="/dev/ttyTHS1", baudrate=9600, timeout=2)
+sp = serial.Serial(port="/dev/ttyTHS1", baudrate=57600)
 time.sleep(1)
 sp.flushOutput()
 sp.flushInput()
@@ -9,9 +9,9 @@ sp.flushInput()
 try:
     if sp.open:
         while True:
-            sp.write("UART test/".encode('utf-8'))
+            sp.write("UART Test/".encode('utf-8'))
             print("sent")
-            time.sleep(5)
+            time.sleep(1)
 
 except KeyboardInterrupt:
     print("Exit")
